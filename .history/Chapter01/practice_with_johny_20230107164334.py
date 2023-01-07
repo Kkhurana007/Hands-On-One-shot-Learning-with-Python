78d34e413b19bc7d888e05f18cb5510aec15bebb
Kunal Khurana <Kunal.khurana.1@ulaval.ca>
2023-01-07 16:44:10 -0500

    add the new files

---
+++ .history/Chapter01/practice_with_johny_20230107164334.py
@@ -0,0 +1,12 @@
+# importing all the necessary libraries needed
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import cross_val_score
+from sklearn.neural_network import MLPClassifier
+
